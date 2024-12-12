1af5f04a60c8f2bc40913b958b26d971409f9c82
Aestheticsuraj234 <jhapriya458@gmail.com>
2024-12-12 15:55:11 +0530

    icons

---
+++ src/app/(protected)/dashboard/[slug]/layout.tsx
@@ -0,0 +1,23 @@
+import Sidebar from '@/components/global/sidebar'
+import React from 'react'
+
+type Props = {
+    children:React.ReactNode
+    params:{slug:string}
+}
+
+const Layout = ({children , params}: Props) => {
+    // Query
+    // WIP:Query client fetch data
+
+  return (
+    <div className='p-3'>
+        {/* sidebar */}
+        <Sidebar slug={params.slug}/>
+        {children}
+        {/* navbar */}
+    </div>
+  )
+}
+
+export default Layout

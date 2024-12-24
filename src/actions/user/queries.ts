416053db504840a3a1751f7cb433d8a4abcd85fa
Aestheticsuraj234 <jhapriya458@gmail.com>
2024-12-24 15:40:39 +0530

    automaiton prifle data and subscription is added

--- src/actions/user/queries.ts
+++ src/actions/user/queries.ts
@@ -1,40 +1,65 @@
-"use server";
+'use server'
 
-import { client } from "@/lib/prisma";
+import { client } from '@/lib/prisma'
 
-export const findUser = async (clerkId:string)=>{
-    return await client.user.findUnique({
-        where:{
-            clerkId:clerkId
+export const findUser = async (clerkId: string) => {
+  return await client.user.findUnique({
+    where: {
+      clerkId,
+    },
+    include: {
+      subscription: true,
+      integrations: {
+        select: {
+          id: true,
+          token: true,
+          expiresAt: true,
+          name: true,
         },
-        include:{
-            subscription:true,
-            integrations:{
-                select:{
-                    id:true,
-                    token:true,
-                    expiresAt:true,
-                    name:true
-                }
-            }
-        }
-    })
+      },
+    },
+  })
 }
 
-export const createUser = async(clerkId:string , firstname:string , lastname:string , email:string)=>{
-    return await client.user.create({
-        data:{
-            clerkId,
-            email,
-            firstname,
-            lastname,
-            subscription:{
-                create:{}
-            }
+export const createUser = async (
+  clerkId: string,
+  firstname: string,
+  lastname: string,
+  email: string
+) => {
+  return await client.user.create({
+    data: {
+      clerkId,
+      firstname,
+      lastname,
+      email,
+      subscription: {
+        create: {},
+      },
+    },
+    select: {
+      firstname: true,
+      lastname: true,
+    },
+  })
+}
+
+export const updateSubscription = async (
+  clerkId: string,
+  props: { customerId?: string; plan?: 'PRO' | 'FREE' }
+) => {
+  return await client.user.update({
+    where: {
+      clerkId,
+    },
+    data: {
+      subscription: {
+        update: {
+          data: {
+            ...props,
+          },
         },
-        select:{
-            firstname:true,
-            lastname:true
-        }
-    })
+      },
+    },
+  })
 }
--- src/components/global/payment-button/index.tsx
+++ src/components/global/payment-button/index.tsx
@@ -1,21 +1,28 @@
 import { Button } from '@/components/ui/button'
+import { useSubscription } from '@/hooks/use-subscription'
+
+import { CreditCardIcon, Loader2 } from 'lucide-react'
 import React from 'react'
 
 type Props = {}
 
 const PaymentButton = (props: Props) => {
-  // WIP: GET THEIR SUBSCRIPTION DETAILS
-  // WIP: LOADING STATE
+  const { onSubscribe, isProcessing } = useSubscription()
   return (
     <Button
-    className="bg-gradient-to-br
-    text-white 
-    rounded-full 
-   from-[#6d60a3] 
-   via-[#9434E6] 
-   font-bold 
-   to-[#CC3BD4]"
-    >Upgrade</Button>
+      disabled={isProcessing}
+      onClick={onSubscribe}
+      className="bg-gradient-to-br
+     text-white 
+     rounded-full 
+    from-[#6d60a3] 
+    via-[#9434E6] 
+    font-bold 
+    to-[#CC3BD4]"
+    >
+      {isProcessing ? <Loader2 className="animate-spin" /> : <CreditCardIcon />}
+      Upgrade
+    </Button>
   )
 }
 
